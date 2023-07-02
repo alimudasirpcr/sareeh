317075bf1b9a5850d8a2192732a42c9539917902
Your Name <theshahzad2019@gmaail.com>
2023-07-02 18:24:18 +0500

    tasks

--- application/views/reports/inputs/specific_entity.php
+++ application/views/reports/inputs/specific_entity.php
@@ -1,6 +1,6 @@
 <div class="form-group">
 	<?php echo form_label($specific_input_label.':', $specific_input_name, array('class'=>'col-sm-3 col-md-3 col-lg-2 control-label ')); ?> 
-	<div class="col-sm-9 col-md-2 col-lg-2">
+	<div class="col-sm-9 col-md-2 col-lg-10">
 		
 		<?php if (isset($search_suggestion_url)) {?>
 			<?php echo form_input(array(
