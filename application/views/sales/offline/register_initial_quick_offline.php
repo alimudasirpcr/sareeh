79bbd8f59b15fa434a566c6e7f937b2a7ff23d8b
alimudasirpcr <alimudasirpcr@gmail.com>
2025-04-12 08:47:48 +0400

    update

--- application/views/sales/offline/register_initial_quick_offline.php
+++ application/views/sales/offline/register_initial_quick_offline.php
@@ -1880,7 +1880,7 @@ $this->load->view("partial/offline_header"); ?>
                             </div>
                             <div class="menu-item">
                             <div class="row">
-                                    <div class="pagination hidden-print alternate text-center" id="pagination" ></div>
+                                    <div class=" hidden-print alternate text-center" id="pagination"  ></div>
                                 </div>
                             </div>
 
