3183c0da494b50c232f3e68377f9ddd55b248aab
alimudasirpcr <alimudasirpcr@gmail.com>
2025-04-20 13:38:55 +0400

    update

--- application/views/sales/offline/register_initial_quick_offline.php
+++ application/views/sales/offline/register_initial_quick_offline.php
@@ -1347,7 +1347,30 @@ $this->load->view("partial/offline_header"); ?>
                                         <i class="ion-ios-settings-strong text-light"></i>
                                     </div>
                                 </div>
+
+                                <?php if($this->config->item('test_mode')): ?>
                                 <div class="d-flex align-items-center flex-row-fluid flex-wrap">
+                                    <div class="flex-grow-1 me-2">
+                                        <a href="<?= base_url(); ?>sales/disable_test_mode" target="_self" id=""
+                                            class="text-gray-800 text-hover-primary fs-6 fw-bold "
+                                            title="Enable Test Mode">Disable Test Mode</a>
+                                    </div>
+                                    <a href="<?= base_url(); ?>sales/disable_test_mode" target="_self" id=""
+                                        class="btn btn-sm btn-icon btn-bg-light btn-active-color-primary w-30px h-30px "
+                                        title="Enable Test Mode"><span class="svg-icon svg-icon-2">
+                                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                                xmlns="http://www.w3.org/2000/svg">
+                                                <rect opacity="0.5" x="18" y="13" width="13" height="2" rx="1"
+                                                    transform="rotate(-180 18 13)" fill="currentColor"></rect>
+                                                <path
+                                                    d="M15.4343 12.5657L11.25 16.75C10.8358 17.1642 10.8358 17.8358 11.25 18.25C11.6642 18.6642 12.3358 18.6642 12.75 18.25L18.2929 12.7071C18.6834 12.3166 18.6834 11.6834 18.2929 11.2929L12.75 5.75C12.3358 5.33579 11.6642 5.33579 11.25 5.75C10.8358 6.16421 10.8358 6.83579 11.25 7.25L15.4343 11.4343C15.7467 11.7467 15.7467 12.2533 15.4343 12.5657Z"
+                                                    fill="currentColor"></path>
+                                            </svg>
+                                        </span></a>
+                                </div>
+
+                                <?php else: ?>
+                                    <div class="d-flex align-items-center flex-row-fluid flex-wrap">
                                     <div class="flex-grow-1 me-2">
                                         <a href="<?= base_url(); ?>sales/enable_test_mode" target="_self" id=""
                                             class="text-gray-800 text-hover-primary fs-6 fw-bold "
@@ -1366,6 +1389,9 @@ $this->load->view("partial/offline_header"); ?>
                                             </svg>
                                         </span></a>
                                 </div>
+
+
+                                <?php endif; ?>
                             </div>
                             <div class="separator separator-dashed my-1"></div>
                             <div class="d-flex flex-stack">
