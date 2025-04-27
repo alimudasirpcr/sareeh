1ae06122dc24972be7b14e0dea351552bbcf142c
alimudasirpcr <alimudasirpcr@gmail.com>
2025-04-27 10:30:43 +0400

    update

--- application/views/sales/offline/js/offline_js.php
+++ application/views/sales/offline/js/offline_js.php
@@ -2287,8 +2287,8 @@ function add_default_tax_to_item() {
 
     console.log("add_default_tax_to_item" , cart);
     cart.items.forEach((item, index) => {
-        console.log("item.taxes" , Object.keys(item.taxes).length);
-            if ( parseInt(item.id) > 0  &&  (!item.taxes || Object.keys(item.taxes).length === 0)) {
+        console.log("item.taxes" , Object.keys(item.taxes).length ,  parseInt(item.id));
+        if (parseInt(item.id) > 0 && (!item.taxes || item.taxes.length === 0)) {
                 console.log("item.taxes added" , taxobj.group);
                 cart.items[index].taxes = taxobj.group;
             }
