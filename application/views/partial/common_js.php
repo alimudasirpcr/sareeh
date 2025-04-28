e4b3192fb619cc8223b0b477ad6c0012d6737f84
alimudasirpcr <alimudasirpcr@gmail.com>
2025-04-28 10:25:43 +0400

    update

--- application/views/partial/common_js.php
+++ application/views/partial/common_js.php
@@ -88,9 +88,7 @@ function doItemSubmit(form, args)
 					$('.commission-container').addClass('hidden');
 					$('.item_kit_item_row').remove();
 				
-					<!-- var selectize = $("#tags")[0].selectize;
-					selectize.clear();
-					selectize.clearOptions(); -->
+					
 			<?php } ?>
 			
 			if(response.quick_edit==null && response.redirect=='' && response.success && response.item_id!=0)
