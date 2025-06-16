4a8d054b2d2e9c0e0d6cfac3cd40f639f9a66774
alimudasirpcr <alimudasirpcr@gmail.com>
2025-06-16 14:15:34 +0400

    update

--- application/views/receivings/receiving_initial.php
+++ application/views/receivings/receiving_initial.php
@@ -630,7 +630,9 @@
 
 		function processSupplierItemsResult(json) {
 			$("#category_item_selection").html('');
-			var back_to_categories_button = $("<div/>").attr('id', 'back_to_suppliers').attr('class', 'category_item register-holder no-image back-to-categories col-md-2 col-sm-3 col-xs-6 ').append('<p>&laquo; ' + <?php echo json_encode(lang('back_to_suppliers')); ?> + '</p>');
+		
+
+			var back_to_categories_button = '<li id="back_to_suppliers"  class=" col-1  category_item back-to-categories register-holder tags-holder h-100px  nav-item mb-3 me-3 me-lg-6 p-0 " role="presentation"><div class="  nav-link d-flex justify-content-between flex-column flex-center overflow-hidden  h-100px py-4 px-0 active " data-bs-toggle="pill"  aria-selected="true" role="tab"><div class="nav-icon"> </div><span class="nav-text text-gray-700 fw-bold fs-6 lh-1"><p> &laquo;' + <?php echo json_encode(lang('back_to_suppliers')); ?> + '</p></span><span class="bullet-custom position-absolute bottom-0 w-100 h-4px bg-primary"></span></div></li>';
 			$("#category_item_selection").append(back_to_categories_button);
 
 			for (var k = 0; k < json.items.length; k++) {
