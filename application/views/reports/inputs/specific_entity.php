e1ceff3737b8d9411461e1491ad0b3f7fdc5c934
alimudasirpcr <alimudasirpcr@gmail.com>
2025-06-29 15:27:45 +0400

    update

--- application/views/reports/inputs/specific_entity.php
+++ application/views/reports/inputs/specific_entity.php
@@ -1,17 +1,18 @@
-<div class="form-group one">
+<div class="form-group">
 	<?php echo form_label($specific_input_label.':', $specific_input_name, array('class'=>'col-sm-3 col-md-3 col-lg-2 form-label ')); ?> 
-	<div class="col-sm-9 col-md-2 col-lg-10 two">
+	<div class="col-sm-9 col-md-2 col-lg-10 ">
 		
 		<?php  
 		
 	
 		if (isset($search_suggestion_url)) {?>
 			<?php echo form_input(array(
-				'class' => 'form-control form-control-solid three  ',
+				'class' => 'form-control form-control-solid  ',
 				'name'=>$specific_input_name,
 				'id'=>$specific_input_name,
-				'size'=>'10'
-			));
+				'size'=>'10',
+				'value'=>$this->input->get($specific_input_name)));
+			
 			?>									
 		<?php } else { ?>
 			<?php echo form_dropdown($specific_input_name,$specific_input_data, $this->input->get($specific_input_name), 'id="'.$specific_input_name.'" class="form-select"'); ?>
@@ -67,6 +68,15 @@
 		<?php
 		}
 		?>
+
+		$(document).on('click', function(e) {
+			// Check if the click is outside the Select2 container
+			if (!$el.is(e.target) && $el.has(e.target).length === 0 && $('.select2-container').has(e.target).length === 0) {
+				$el.select2('close');
+			}
+		});
+
+
 	});
 </script>
 
